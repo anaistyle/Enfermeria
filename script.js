f29db455ceca56477637c12a9750ba42da6a84b8
anaistyle <anaisjara2@gmail.com>
2025-07-31 22:25:25 -0400

    script.js

--- script.js
+++ script.js
@@ -1,134 +1,98 @@
-// script.js
-
 const ramos = [
-  // Año 1 - Semestre 1
-  { nombre: "Biología Celular Básica Enfermería", semestre: 1, creditos: 3, requisitos: [], id: "bio-cel" },
-  { nombre: "Psicología General Y Evolutiva", semestre: 1, creditos: 4, requisitos: [], id: "psico-gen" },
-  { nombre: "Socioantropologia I", semestre: 1, creditos: 2, requisitos: [], id: "socio1" },
-  { nombre: "Fundamentos De Enfermería", semestre: 1, creditos: 2, requisitos: [], id: "funda-enf" },
-  { nombre: "Promoción de la Salud", semestre: 1, creditos: 3, requisitos: [], id: "promo-salud" },
-  { nombre: "Inglés Comunicativo I", semestre: 1, creditos: 3, requisitos: [], id: "ingles1" },
+  // 1° AÑO - SEMESTRE I
+  { nombre: "Biología Celular Básica Enfermería", anio: 1, semestre: 1, creditos: 3, prerrequisitos: [] },
+  { nombre: "Psicología General y Evolutiva", anio: 1, semestre: 1, creditos: 4, prerrequisitos: [] },
+  { nombre: "Sociantropología I", anio: 1, semestre: 1, creditos: 2, prerrequisitos: [] },
+  { nombre: "Fundamentos de Enfermería", anio: 1, semestre: 1, creditos: 4, prerrequisitos: [] },
+  { nombre: "Promoción de la Salud", anio: 1, semestre: 1, creditos: 3, prerrequisitos: [] },
+  { nombre: "Inglés Comunicativo I Nivel Principiante", anio: 1, semestre: 1, creditos: 2, prerrequisitos: [] },
 
-  // Año 1 - Semestre 2
-  { nombre: "Histología Humana - Enfermeria", semestre: 2, creditos: 3, requisitos: ["bio-cel"], id: "histo" },
-  { nombre: "Anatomía Descriptiva - Enfermería", semestre: 2, creditos: 4, requisitos: ["bio-cel"], id: "anato" },
-  { nombre: "Prevención En Salud", semestre: 2, creditos: 3, requisitos: ["promo-salud", "funda-enf"], id: "prev-salud" },
-  { nombre: "Socioantropología II", semestre: 2, creditos: 2, requisitos: ["socio1"], id: "socio2" },
-  { nombre: "Química General Y Básica", semestre: 2, creditos: 6, requisitos: [], id: "quimica" },
-  { nombre: "Inglés Comunicativo II", semestre: 2, creditos: 3, requisitos: ["ingles1"], id: "ingles2" },
+  // 1° AÑO - SEMESTRE II
+  { nombre: "Histología Humana-Enfermería", anio: 1, semestre: 2, creditos: 3, prerrequisitos: [] },
+  { nombre: "Química General y Orgánica Básica", anio: 1, semestre: 2, creditos: 3, prerrequisitos: [] },
+  { nombre: "Sociantropología II", anio: 1, semestre: 2, creditos: 2, prerrequisitos: ["Sociantropología I"] },
+  { nombre: "Prevención en Salud", anio: 1, semestre: 2, creditos: 3, prerrequisitos: [] },
+  { nombre: "Anatomía Descriptiva Enfermería", anio: 1, semestre: 2, creditos: 3, prerrequisitos: [] },
+  { nombre: "Inglés Comunicativo II Nivel Elemental", anio: 1, semestre: 2, creditos: 2, prerrequisitos: ["Inglés Comunicativo I Nivel Principiante"] },
 
-  // Año 2 - Semestre 3
-  { nombre: "Fisiología Enfermería", semestre: 3, creditos: 4, requisitos: ["histo", "anato", "quimica"], id: "fisio" },
-  { nombre: "Bioquímica - Enfermería", semestre: 3, creditos: 3, requisitos: ["quimica"], id: "bioq" },
-  { nombre: "Enfermería En Salud Familiar Y Comunitaria", semestre: 3, creditos: 4, requisitos: ["prev-salud", "socio2"], id: "fam1" },
-  { nombre: "Fundamentos De La Gestión Del Cuidado De Enfermería", semestre: 3, creditos: 4, requisitos: ["histo", "anato", "prev-salud"], id: "fundagestion" },
-  { nombre: "Fundamentos De Ética En Enfermería", semestre: 3, creditos: 2, requisitos: [], id: "etica" },
-  { nombre: "Complementaria I", semestre: 3, creditos: 2, requisitos: [], id: "comp1" },
+  // 2° AÑO - SEMESTRE III
+  { nombre: "Fisiología Enfermería", anio: 2, semestre: 3, creditos: 3, prerrequisitos: [] },
+  { nombre: "Bioquímica Enfermería", anio: 2, semestre: 3, creditos: 3, prerrequisitos: ["Química General y Orgánica Básica"] },
+  { nombre: "Enfermería en Salud Familiar y Comunitaria I", anio: 2, semestre: 3, creditos: 4, prerrequisitos: ["Fundamentos de Enfermería"] },
+  { nombre: "Fundamentos de la Gestión del Cuidado de Enfermería", anio: 2, semestre: 3, creditos: 3, prerrequisitos: [] },
+  { nombre: "Fundamentos de Ética en Enfermería", anio: 2, semestre: 3, creditos: 2, prerrequisitos: [] },
+  { nombre: "Complementario", anio: 2, semestre: 3, creditos: 2, prerrequisitos: [] },
 
-  // Año 2 - Semestre 4
-  { nombre: "Fisiopatología Enfermería", semestre: 4, creditos: 4, requisitos: ["fisio", "bioq"], id: "fisio-pato" },
-  { nombre: "Farmacologia - Enfermeria", semestre: 4, creditos: 3, requisitos: ["fisio", "bioq"], id: "farmaco" },
-  { nombre: "Gestión Del Cuidado De Enfermería En El Adulto I", semestre: 4, creditos: 4, requisitos: ["fisio", "bioq", "fam1", "fundagestion", "etica"], id: "adulto1" },
-  { nombre: "Enfermería En El Adulto", semestre: 4, creditos: 5, requisitos: ["fisio", "fundagestion"], id: "enf-adulto" },
-  { nombre: "Interacción Humana", semestre: 4, creditos: 2, requisitos: ["etica"], id: "interaccion" },
-  { nombre: "Microbiología Enfermería Obstetricia", semestre: 4, creditos: 4, requisitos: ["fisio", "bioq"], id: "micro" },
+  // 2° AÑO - SEMESTRE IV
+  { nombre: "Fisiopatología Enfermería", anio: 2, semestre: 4, creditos: 3, prerrequisitos: ["Fisiología Enfermería"] },
+  { nombre: "Farmacología Enfermería", anio: 2, semestre: 4, creditos: 3, prerrequisitos: ["Bioquímica Enfermería"] },
+  { nombre: "Gestión del Cuidado de Enfermería en el Adulto I", anio: 2, semestre: 4, creditos: 4, prerrequisitos: ["Fundamentos de Enfermería"] },
+  { nombre: "Metodología de la Investigación Cuantitativa", anio: 2, semestre: 4, creditos: 3, prerrequisitos: [] },
+  { nombre: "Enfermería en el Adulto", anio: 2, semestre: 4, creditos: 3, prerrequisitos: [] },
+  { nombre: "Microbiología Enfermería Obstetricia", anio: 2, semestre: 4, creditos: 3, prerrequisitos: [] },
+  { nombre: "Interacción Humana", anio: 2, semestre: 4, creditos: 2, prerrequisitos: [] },
+  { nombre: "Complementario", anio: 2, semestre: 4, creditos: 2, prerrequisitos: [] },
 
-  // Año 3 - Semestre 5
-  { nombre: "Gerencia En Salud Y Enfermería", semestre: 5, creditos: 3, requisitos: ["adulto1", "enf-adulto"], id: "gerencia" },
-  { nombre: "Gestión Del Cuidado De Enfermería En El Adulto II", semestre: 5, creditos: 6, requisitos: ["adulto1", "enf-adulto", "fisio-pato", "farmaco", "micro", "interaccion"], id: "adulto2" },
-  { nombre: "Metodología De La Investigación Cuantitativa", semestre: 5, creditos: 3, requisitos: [], id: "cuanti" },
-  { nombre: "Comunicación Y Liderazgo", semestre: 5, creditos: 2, requisitos: ["interaccion"], id: "liderazgo" },
-  { nombre: "Complementaria 2", semestre: 5, creditos: 2, requisitos: [], id: "comp2" },
+  // 3° AÑO - SEMESTRE V
+  { nombre: "Gerencia en Salud y Enfermería", anio: 3, semestre: 5, creditos: 3, prerrequisitos: [] },
+  { nombre: "Gestión del Cuidado de Enfermería en el Adulto II", anio: 3, semestre: 5, creditos: 4, prerrequisitos: ["Gestión del Cuidado de Enfermería en el Adulto I"] },
+  { nombre: "Metodología de la Investigación Cualitativa", anio: 3, semestre: 5, creditos: 3, prerrequisitos: ["Metodología de la Investigación Cuantitativa"] },
+  { nombre: "Comunicación y Liderazgo", anio: 3, semestre: 5, creditos: 2, prerrequisitos: [] },
 
-  // Año 3 - Semestre 6
-  { nombre: "Gestión De Proyectos", semestre: 6, creditos: 3, requisitos: ["cuanti", "gerencia", "adulto2"], id: "proyectos" },
-  { nombre: "Enfermería Y Gestión Del Cuidado En El Adulto Mayor", semestre: 6, creditos: 5, requisitos: ["gerencia", "adulto2"], id: "adulto-mayor" },
-  { nombre: "Metodología De La Investigación Cualitativa", semestre: 6, creditos: 3, requisitos: [], id: "cuali" },
-  { nombre: "Enfermería Y Gestión Del Cuidado En El Niño Y Adolescente I", semestre: 6, creditos: 5, requisitos: ["adulto2"], id: "nino1" },
-  { nombre: "Bioética Y Enfermería", semestre: 6, creditos: 2, requisitos: [], id: "bioetica" },
-  { nombre: "Enfermería En Salud Mental", semestre: 6, creditos: 4, requisitos: ["liderazgo"], id: "salud-mental" },
+  // 3° AÑO - SEMESTRE VI
+  { nombre: "Gestión de Proyecto", anio: 3, semestre: 6, creditos: 3, prerrequisitos: [] },
+  { nombre: "Enfermería y Gestión del Cuidado en el Adulto Mayor", anio: 3, semestre: 6, creditos: 4, prerrequisitos: [] },
+  { nombre: "Enfermería y Gestión del Cuidado en el Niño y Adolescente I", anio: 3, semestre: 6, creditos: 4, prerrequisitos: [] },
+  { nombre: "Enfermería en Salud Mental", anio: 3, semestre: 6, creditos: 3, prerrequisitos: [] },
+  { nombre: "Bioética y Enfermería", anio: 3, semestre: 6, creditos: 2, prerrequisitos: [] },
 
-  // Año 4 - Semestre 7
-  { nombre: "Enfermería En Urgencias Y Desastres", semestre: 7, creditos: 3, requisitos: ["adulto-mayor", "nino1", "salud-mental"], id: "urgencias" },
-  { nombre: "Enfermería Basada En La Evidencia", semestre: 7, creditos: 3, requisitos: ["cuali"], id: "ebe" },
-  { nombre: "Enfermería En Salud Familiar Y Comunitaria II", semestre: 7, creditos: 4, requisitos: ["adulto-mayor", "nino1"], id: "fam2" },
-  { nombre: "Enfermería Y Gestión Del Cuidado En El Niño Y Adolescente II", semestre: 7, creditos: 6, requisitos: ["nino1"], id: "nino2" },
-  { nombre: "Gestión Del Cuidado De Enfermería En Salud Mental", semestre: 7, creditos: 3, requisitos: ["salud-mental"], id: "gestion-mental" },
+  // 4° AÑO - SEMESTRE VII
+  { nombre: "Enfermería en Urgencias y Desastres", anio: 4, semestre: 7, creditos: 4, prerrequisitos: [] },
+  { nombre: "Enfermería Basada en la Evidencia", anio: 4, semestre: 7, creditos: 3, prerrequisitos: [] },
+  { nombre: "Enfermería en Salud Familiar y Comunitaria II", anio: 4, semestre: 7, creditos: 4, prerrequisitos: ["Enfermería en Salud Familiar y Comunitaria I"] },
+  { nombre: "Enfermería y Gestión del Cuidado en el Niño y Adolescente II", anio: 4, semestre: 7, creditos: 4, prerrequisitos: ["Enfermería y Gestión del Cuidado en el Niño y Adolescente I"] },
+  { nombre: "Gestión del Cuidado de Enfermería en Salud Mental", anio: 4, semestre: 7, creditos: 3, prerrequisitos: ["Enfermería en Salud Mental"] },
 
-  // Año 4 - Semestre 8
-  { nombre: "Gestión Del Cuidado De Enfermería En Urgencias Y Desastres", semestre: 8, creditos: 6, requisitos: ["urgencias", "nino2", "gestion-mental"], id: "gestion-urg" },
-  { nombre: "Calidad Y Seguridad En Salud", semestre: 8, creditos: 3, requisitos: ["ebe"], id: "calidad" },
-  { nombre: "Gestión Del Cuidado De Enfermería En Salud Familiar Y Comunitaria", semestre: 8, creditos: 6, requisitos: ["fam2", "nino2", "gestion-mental"], id: "gestion-fam" },
+  // 4° AÑO - SEMESTRE VIII
+  { nombre: "Gestión del Cuidado de Enfermería en Urgencias y Desastres", anio: 4, semestre: 8, creditos: 4, prerrequisitos: ["Enfermería en Urgencias y Desastres"] },
+  { nombre: "Calidad y Seguridad en Salud", anio: 4, semestre: 8, creditos: 3, prerrequisitos: [] },
+  { nombre: "Gestión del Cuidado Enfermería en Salud Familiar y Comunitaria", anio: 4, semestre: 8, creditos: 4, prerrequisitos: ["Enfermería en Salud Familiar y Comunitaria II"] },
 
-  // Año 5 - Semestre 9
-  { nombre: "Internado Extramural", semestre: 9, creditos: 20, requisitos: [], id: "internado-ext" },
-
-  // Año 5 - Semestre 10
-  { nombre: "Internado Intramural", semestre: 10, creditos: 20, requisitos: [], id: "internado-int" },
-  { nombre: "Examen De Titulación", semestre: 10, creditos: 0, requisitos: [], id: "titulacion" },
+  // 5° AÑO
+  { nombre: "Internado Extramural", anio: 5, semestre: 9, creditos: 15, prerrequisitos: [] },
+  { nombre: "Internado Intramural", anio: 5, semestre: 10, creditos: 15, prerrequisitos: [] }
 ];
 
-const totalCreditos = 305;
-let creditosCompletados = 0;
+const grid = document.querySelector('.grid');
 
-const estadoRamos = {};
-ramos.forEach(r => estadoRamos[r.id] = false);
+function crearRamo(ramo) {
+  const div = document.createElement('div');
+  div.className = 'ramo';
+  div.dataset.nombre = ramo.nombre;
+  div.dataset.creditos = ramo.creditos;
+  div.dataset.aprobado = 'false';
+  div.innerHTML = `${ramo.nombre}<div class="creditos">${ramo.creditos} créditos</div>`;
 
-function crearMalla() {
-  const malla = document.getElementById("malla");
-  const semestres = [...new Set(ramos.map(r => r.semestre))];
-
-  semestres.forEach(sem => {
-    const columna = document.createElement("div");
-    columna.className = "semestre";
-    const titulo = document.createElement("h2");
-    titulo.textContent = `Semestre ${sem}`;
-    columna.appendChild(titulo);
-
-    ramos.filter(r => r.semestre === sem).forEach(ramo => {
-      const div = document.createElement("div");
-      div.className = "ramo bloqueado";
-      div.id = ramo.id;
-      div.innerHTML = `
-        <div>${ramo.nombre}</div>
-        <div class="creditos">${ramo.creditos} créditos</div>
-      `;
-
-      div.addEventListener("click", () => aprobarRamo(ramo));
-
-      columna.appendChild(div);
-    });
-
-    malla.appendChild(columna);
-  });
-  actualizarEstado();
-}
-
-function aprobarRamo(ramo) {
-  const div = document.getElementById(ramo.id);
-  if (!div.classList.contains("bloqueado")) {
-    const aprobado = div.classList.toggle("aprobado");
-    estadoRamos[ramo.id] = aprobado;
-    creditosCompletados += aprobado ? ramo.creditos : -ramo.creditos;
-    actualizarEstado();
-  }
-}
-
-function actualizarEstado() {
-  ramos.forEach(r => {
-    const div = document.getElementById(r.id);
-    const requisitosCumplidos = r.requisitos.every(req => estadoRamos[req]);
-    if (requisitosCumplidos) {
-      div.classList.remove("bloqueado");
+  div.addEventListener('click', () => {
+    const aprobados = ramo.prerrequisitos.every(pr => document.querySelector(`[data-nombre='${pr}']`)?.classList.contains('aprobado'));
+    if (aprobados) {
+      div.classList.toggle('aprobado');
+      actualizarProgreso();
     } else {
-      div.classList.add("bloqueado");
-      div.classList.remove("aprobado");
-      estadoRamos[r.id] = false;
+      alert("Debes aprobar los prerrequisitos primero");
     }
   });
-  const porcentaje = (creditosCompletados / totalCreditos) * 100;
-  document.getElementById("progreso-barra").style.width = `${porcentaje}%`;
-  document.getElementById("progreso-texto").textContent = `Progreso: ${creditosCompletados} créditos / ${totalCreditos}`;
+
+  const col = (ramo.semestre + (ramo.anio - 1) * 2);
+  div.style.gridColumn = col;
+
+  grid.appendChild(div);
 }
 
-window.onload = crearMalla;
+function actualizarProgreso() {
+  const total = ramos.length;
+  const aprobados = document.querySelectorAll('.ramo.aprobado').length;
+  document.getElementById('progreso').value = (aprobados / total) * 100;
+}
+
+ramos.forEach(crearRamo);
